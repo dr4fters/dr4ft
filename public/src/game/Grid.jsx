3c136fbe38ff8c590f1e1eaefb3ddc06b7992a89
HerveH44 <herve.huneau@gmail.com>
2017-11-03 11:42:14 +0100

    fix&ref: modify cards function

--- public/src/game/Grid.jsx
+++ public/src/game/Grid.jsx
@@ -1,7 +1,7 @@
-import _ from '../../lib/utils'
-import App from '../app'
-import {getZone} from '../cards'
-import {Spaced} from '../utils'
+import _ from "../../lib/utils"
+import App from 'Src/app'
+import {getZone} from 'Src/cards'
+import {Spaced} from 'Src/utils'
 import React from "react"
 
 const Grid = ({zones}) => (
@@ -15,32 +15,45 @@ const zone = (zoneName) => {
   const values = _.values(zone)
   const cards = _.flat(values)
 
-  const isAutopickable = card => zoneName === 'pack' && card.isAutopick
-  const zoneHelper = `${cards.length} 
-          ${zoneName === 'pack' ? 
-              ' /  ' + cards[0].packSize.toString() : 
-              ""} 
+  const zoneHelper = `${cards.length}
+          ${zoneName === 'pack' ?
+              ' /  ' + cards[0].packSize.toString() :
+              ""}
           ${cards.length === 1 ? 'card' : 'cards' }`
 
   return (
-    <div className='zone'>
+    <div className='zone' key={_.uid()}>
       <h1>
         <Spaced elements={[zoneName, zoneHelper]}/>
       </h1>
-      {cards.map(card => <span
-        className={`card ${isAutopickable(card)
-        ? 'autopick-card '
-        : ''} card ${card.foil
-          ? 'foil-card '
-          : ''}`}
-        title={isAutopickable(card)
-        ? 'This card will be automatically picked if your time expires.'
-        : ''}
-        onClick={App._emit('click', zoneName, card.name)}>
-        <img src={card.url} alt={card.name}/>
-      </span>)}
+      <Cards zoneName={zoneName} cards={cards} />
     </div>
   )
 }
+
+const Cards = ({cards, zoneName}) => {
+  const doCard = (card) => {
+    const isAutopickable = card => zoneName === 'pack' && card.isAutopick
+    const className =
+    `card ${isAutopickable(card) ? 'autopick-card ' : ''}
+    card ${card.foil ? 'foil-card ' : ''}`
+
+    const title =
+    isAutopickable(card)
+        ? 'This card will be automatically picked if your time expires.'
+        : ''
+    return (
+      <span key={_.uid()}
+        className={className}
+        title={title}
+        onClick={App._emit('click', zoneName, card.name)}>
+        <img src={card.url} alt={card.name}/>
+      </span>
+    )
+  }
+
+  return cards.map(doCard)
+}
+
 
 export default Grid
