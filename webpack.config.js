9b721f2ec5ab1dee19cedd942ddf7f003b4d15ce
Hervé Huneau <herve.huneau@emaze.net>
2017-11-13 09:52:46 +0100

    Ref: delete unused vars

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = {
   devtool: 'cheap-eval-source-map',
