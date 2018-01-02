2b3cae38e39f9219b01e020b99476fe600613d8d
Dave <tritochs@gmail.com>
2018-01-02 09:07:57 -0600

    Local score db (#71)
    * Move score db to local and update with current data. Score db in following commit
    
    * scores file current 12/29/17

--- src/make/score.js
+++ src/make/score.js
@@ -1,26 +1,16 @@
 var fs = require('fs')
 var fetch = require('node-fetch')
 var {Cards} = require('../data')
+var scores = JSON.parse(fs.readFileSync("./data/scores.json"))
 
-var URL = 'https://aeos.cloudant.com/draft/_design/draft/_view/score?group=true'
-
-fetch(URL)
-.then(res => {
-  if (res.ok)
-    return res.json()
-  throw Error('not ok')
-})
-.then(data => {
-  for (var row of data.rows) {
+for (var row of scores.rows) {
     var {key, value} = row
     var lc = key.toLowerCase()
     // TODO scrub the db
     if (!(lc in Cards))
-      continue
+        continue
 
     Cards[lc].score = value.sum / value.count
-  }
+}
 
-  fs.writeFileSync('data/cards.json', JSON.stringify(Cards, null, 2))
-})
-.catch(console.log)
+fs.writeFileSync('data/cards.json', JSON.stringify(Cards, null, 2))
