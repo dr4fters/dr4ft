01c892392c5a8ec0fec463f79b27f3b8d36f51b4
HerveH44 <herve.huneau@gmail.com>
2018-02-19 21:47:33 +0100

    fix version and config.client typos

--- public/src/lobby/Version.jsx
+++ public/src/lobby/Version.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Version({version}) {
     return (
         <p>Running Version{' '}
-            <a href="https://github.com/dr4fters/dr4ft/commit/{version}">
+            <a href={`https://github.com/dr4fters/dr4ft/commit/${version}`}>
                 {version}
             </a>
         </p>
