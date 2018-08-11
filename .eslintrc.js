e6b6fd27da6ece0ac96df18a2f26d6376830d998
tooomm <tooomm@users.noreply.github.com>
2018-08-11 12:33:19 +0200

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
       "plugin:react/recommended"
     ],
     "parserOptions": {
-        "ecmaVersion": 2018
+        "ecmaVersion": 2018,
         "ecmaFeatures": {
             "jsx": true
         },
