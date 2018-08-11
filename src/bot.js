b2ebe79a9183b38144a909d2e7866bcca7c13e80
tooomm <tooomm@users.noreply.github.com>
2018-08-11 12:22:32 +0200

    run + fix eslint

--- src/bot.js
+++ src/bot.js
@@ -1,51 +1,51 @@
-var _ = require('./_')
-var {EventEmitter} = require('events')
+var _ = require("./_");
+var {EventEmitter} = require("events");
 
 module.exports = class extends EventEmitter {
   constructor() {
-    super()
+    super();
     Object.assign(this, {
       isBot: true,
       isConnected: true,
-      name: 'bot',
+      name: "bot",
       packs: [],
       time: 0,
       cap: {
         packs: {}
       },
       picks: []
-    })
+    });
   }
   get isActive() {
-    return false
+    return false;
   }
   getPack(pack) {
-    var score = 99
-    var index = 0
-    var cardcount = 0
-    var scoredcards = 0
+    var score = 99;
+    var index = 0;
+    var cardcount = 0;
+    var scoredcards = 0;
     pack.forEach((card, i) => {
       if (card.score) {
         if (card.score < score) {
-          score = card.score
-          index = i
+          score = card.score;
+          index = i;
         }
-      scoredcards = scoredcards + 1
+        scoredcards = scoredcards + 1;
       }
-      cardcount = i
-    })
+      cardcount = i;
+    });
     //if 50% of cards doesn't have a score, we're going to pick randomly
     if (scoredcards / cardcount < .5) {
-      var randpick = _.rand(cardcount)
-      this.picks.push(pack[randpick].name)
-      pack.splice(randpick, 1)
+      var randpick = _.rand(cardcount);
+      this.picks.push(pack[randpick].name);
+      pack.splice(randpick, 1);
     }
     else {
-      this.picks.push(pack[index].name)
-      pack.splice(index, 1)
+      this.picks.push(pack[index].name);
+      pack.splice(index, 1);
     }
-    this.emit('pass', pack)
+    this.emit("pass", pack);
   }
   send(){}
   err(){}
-}
+};
