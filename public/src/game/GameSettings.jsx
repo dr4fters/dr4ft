c3ce7f9ba20c03f512354ab34065f7d19b28419b
Hervé Huneau <herve.huneau@gmail.com>
2019-02-22 18:48:37 +0100

    Allow user to set card's size (to decrease loading image time) (#343)
    * set default cardSize to small
    
    * add radio button to check card size
    
    * set card quality to normal by default
    
    * change CardsImage to CardsImageQuality

--- public/src/game/GameSettings.jsx
+++ public/src/game/GameSettings.jsx
@@ -27,6 +27,7 @@ const GameSettings = () => (
           <Checkbox side="left" text="Column view" link="cols" />
         </div>
         <SortCards />
+        <CardsImageQuality />
       </span>
     </fieldset>
   </div>
@@ -47,6 +48,18 @@ const SortCards = () => (
           {sort}
         </label>
       )}
+    </div>
+  </div>
+);
+
+const CardsImageQuality = () => (
+  <div className="settings-cards-size">
+    Card image quality:
+    <div className='connected-container'>
+      {["small", "normal", "large"].map((size, index) => <label key={index} className='radio-label connected-component'>
+        <input checked={size === App.state.cardSize} className='radio-input' name='card-size' onChange={e => App.save("cardSize", e.currentTarget.value)} type='radio' value={size} />
+        {size}
+      </label>)}
     </div>
   </div>
 );
