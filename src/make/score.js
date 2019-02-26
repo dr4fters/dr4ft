982e9175efb2bde000cfb1e48a59ebbf687ebfb1
Hervé Huneau <herve.huneau@emaze.net>
2019-02-26 17:20:57 +0100

    address TODOs and cleanup dependencies

--- src/make/score.js
+++ src/make/score.js
@@ -1,15 +1,28 @@
-var fs = require("fs");
-var {Cards} = require("../data");
-var scores = JSON.parse(fs.readFileSync("./data/scores.json"));
+const fs = require("fs");
+const logger = require("../logger");
+const { getCards, writeCards } = require("../data");
+const SCORE_PATH = "./data/scores.json";
 
-for (var row of scores.rows) {
-  var {key, value} = row;
-  var lc = key.toLowerCase();
-  // TODO scrub the db
-  if (!(lc in Cards))
-    continue;
+if (!fs.existsSync(SCORE_PATH)) {
+  logger.error(`Score file not present at ${SCORE_PATH}. Aborting`);
+} else {
+  try {
+    const scores = JSON.parse(fs.readFileSync(SCORE_PATH));
+    const Cards = getCards();
 
-  Cards[lc].score = value.sum / value.count;
+    for (const row of scores.rows) {
+      const { key, value: { sum, count } } = row;
+      const lc = key.toLowerCase();
+      if (!(lc in Cards)) {
+        logger.error(`Can't find card ${lc} in our DB`);
+        continue;
+      }
+      Cards[lc].score = sum / count;
+    }
+
+    writeCards(Cards);
+  } catch(err) {
+    logger.err(`Error while updating scores: ${err.stack}`);
+  }
+ 
 }
-
-fs.writeFileSync("data/cards.json", JSON.stringify(Cards, null, 2));
