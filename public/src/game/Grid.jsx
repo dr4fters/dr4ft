9f7c97dd4423a18a94e6ff77c632ad7546743b17
Hervé Huneau <herve.huneau@emaze.net>
2019-02-26 16:20:06 +0100

    handle double faced cards and flipped card

--- public/src/game/Grid.jsx
+++ public/src/game/Grid.jsx
@@ -40,38 +40,47 @@ const zone = (zoneName, index) => {
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      url: props.card.url
-    };
     this.onMouseEnter = this.onMouseEnter.bind(this);
     this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.getClassName = this.getClassName.bind(this);
+    this.state = {
+      url: props.card.url,
+      className: this.getClassName()
+    };
+  }
+
+  getClassName() {
+    const {card, zoneName} = this.props;
+    const isAutopickable = zoneName === "pack" && card.isAutopick;
+    return `card 
+    ${isAutopickable ? "autopick-card " : ""}
+    ${card.foil ? "foil-card " : ""}`;
   }
   onMouseEnter() {
     if(this.props.card.isDoubleFaced) {
       this.setState({
-        url: this.props.card.flippedCardURL
+        url: this.props.card.flippedCardURL,
+        className: `${this.getClassName()} ${this.props.card.layout === "flip" ? "flipped" : ""}` 
       });
     }
   }
   onMouseLeave() {
     if(this.props.card.isDoubleFaced) {
       this.setState({
-        url: this.props.card.url
+        url: this.props.card.url,
+        className: this.getClassName()
       });
     }
   }
   render() {
     const {card, zoneName} = this.props;
     const isAutopickable = zoneName === "pack" && card.isAutopick;
-    const className =
-    `card ${isAutopickable ? "autopick-card " : ""}
-    card ${card.foil ? "foil-card " : ""}`;
     const title
     = isAutopickable
       ? "This card will be automatically picked if your time expires."
       : "";
     return (
-      <span className={className}
+      <span className={this.state.className}
         title={title}
         onClick={App._emit("click", zoneName, card.name)}
         onMouseEnter={this.onMouseEnter}
