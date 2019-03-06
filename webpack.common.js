fb3c3f6fd92a1aee6069b37832e97aa9a73d34fd
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-03-05 22:36:25 -0500

    remove webpack plugin import

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
