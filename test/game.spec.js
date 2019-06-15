cac0dbeafd239b0523d30f8125a687a8e53a5563
Hervé Huneau <herve.huneau@gmail.com>
2019-06-16 00:28:48 +0200

    fix: set rounds to sets length (#510)

--- test/game.spec.js
+++ test/game.spec.js
@@ -14,4 +14,15 @@ describe("Acceptance tests for Game class", () => {
       assert.equal(got.type, "draft");
     });
   });
+  describe("can make a draft with 4 sets", () => {
+    it("should return a a game with 4 round", async () => {
+      const gameParams = {
+        hostId: "1", title: "test game", seats: 8, type: "draft", sets: ["GRN", "GRN", "GRN", "GRN"],
+        cube: null, isPrivate: true, modernOnly: false, totalChaos: false
+      };
+
+      const got = new Game(gameParams);
+      assert.equal(got.rounds, 4);
+    });
+  });
 });
