aa583ee445b23ceeff0f8081b32954786b87233e
Hervé Huneau <herve.huneau@emaze.net>
2019-12-11 17:31:50 +0100

    first implementation of default confiig (broken)

---
+++ config/app.js
@@ -0,0 +1,16 @@
+const joi = require("@hapi/joi");
+
+const envVarsSchema = joi.object({
+  PORT: joi.number()
+    .default(1337),
+  HOST: joi.string()
+    .default("localhost"),
+}).unknown()
+  .required();
+
+const { error, value: envVars } = envVarsSchema.validate(process.env);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = envVars;
