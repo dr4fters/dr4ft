c0eaa45d093dddf5007da0ea8c6b7282513eb88d
HerveH44 <herve.huneau@gmail.com>
2019-12-17 00:39:46 +0100

    implement XML parsing

--- src/api/sets.js
+++ src/api/sets.js
@@ -18,50 +18,59 @@ setsRouter
     let file = req.files.filepond;
     const content = file.data.toString();
 
-    if (/xml$/.test(file.name)) {
-      parser.parse(content);
-    }
-
-    try {
-      const json = JSON.parse(content);
-      const newCards = getCards();
-      const sets = getSets();
-
-      // Avoid overwriting existing sets
-      if ((json.code in sets)) {
-        // Unless it's a custom set. In this case, we allow overriding
-        if (sets[json.code].type != CUSTOM_TYPE) {
-          logger.warn(`Set existing already. Not saving agin set with code "${json.code}" to database`);
-          res.status(400).json(`Set existing already. Not saving agin set with code "${json.code}" to database`);
-          return;
-        } else {
-          logger.info(`Custom set ${json.code} already existing. Overriding with new file...`);
-        }
+    if (/\.xml$/.test(file.name)) {
+      try {
+        Object.values(parser.parse(content)).forEach(json => {
+          integrateJson(json, res);
+        });
+      } catch(err) {
+        logger.error(`Could not parse XML file: ${err}`);
+        res.status(400).json(`the xml submitted is not valid: ${err}`);
+        return;
       }
-
-      const [parsedSet, parsedCards] = doSet(json, {}, newCards);
-      parsedSet.type = CUSTOM_TYPE; //Force set as custom
-
-      logger.info(`adding new set with code "${json.code}" to database`);
-      sets[json.code] = parsedSet;
-      writeSets(sets);
-      Sock.broadcast("set", { availableSets: getPlayableSets(), latestSet: getLatestReleasedSet() });
-
-      writeCards(parsedCards);
-
-      //Moving custom set to custom directory
-      fs.writeFile(`data/custom/${json.code}.json`, JSON.stringify(json), (err) => {
-        if (err) {
-          logger.error(`Could not save file ${json.code}.json. ${err}`);
-          return res.status(500).send(err);
-        }
-        logger.info(`Saved custom set as file ${json.code}.json`);
-        res.json({ "message": "set integrated successfully" });
-      });
-    } catch (err) {
-      logger.error(`Could not parse JSON file because ${err}`);
-      res.status(400).json(`the json submitted is not valid: ${err}`);
+    } else if( /\.json/.test(file.name)) {
+      try {
+        const json = JSON.parse(content);
+        integrateJson(json, res);
+      } catch (err) {
+        logger.error(`Could not parse JSON file because ${err}`);
+        res.status(400).json(`the json submitted is not valid: ${err}`);
+      }
     }
+    res.json({ "message": "file integrated successfully" });
   });
+
+function integrateJson(json) {
+  const newCards = getCards();
+  const sets = getSets();
+
+  // Avoid overwriting existing sets
+  if ((json.code in sets)) {
+  // Unless it's a custom set. In this case, we allow overriding
+    if (sets[json.code].type != CUSTOM_TYPE) {
+      throw new Error(`Set existing already. Not saving agin set with code "${json.code}" to database`);
+    } else {
+      logger.info(`Custom set ${json.code} already existing. Overriding with new file...`);
+    }
+  }
+
+  const [parsedSet, parsedCards] = doSet(json, {}, newCards);
+  parsedSet.type = CUSTOM_TYPE; //Force set as custom
+
+  logger.info(`adding new set with code "${json.code}" to database`);
+  sets[json.code] = parsedSet;
+  writeSets(sets);
+  Sock.broadcast("set", { availableSets: getPlayableSets(), latestSet: getLatestReleasedSet() });
+
+  writeCards(parsedCards);
+
+  //Moving custom set to custom directory
+  fs.writeFile(`data/custom/${json.code}.json`, JSON.stringify(json), (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+    logger.info(`Saved custom set as file ${json.code}.json`);
+  });
+}
 
 module.exports = setsRouter;
--- src/make/xml/parser.js
+++ src/make/xml/parser.js
@@ -1,51 +1,83 @@
-const libxml = require("libxmljs");
-const fs = require("fs");
-const path = require("path");
-
-const schema = fs.readFileSync(path.join(__dirname, "cards.xsd"));
-const xsdDoc = libxml.parseXml(schema);
+const parser = require("fast-xml-parser");
 
 function parse(content) {
-  const parsedContent = libxml.parseXml(content);
-  parsedContent.validate(xsdDoc);
-  if (parsedContent.validationErrors.length) {
-    throw new Error(parsedContent.validationErrors);
+  const parsedContent = parser.parse(content, {ignoreAttributes: false, attributeNamePrefix: "", textNodeName: "text"});
+  const root = parsedContent.cockatrice_carddatabase;
+  if (!root) {
+    throw new Error("root node <cockatrice_carddatabase> must be present");
   }
-  const {cockatrice_carddatabase} = parsedContent;
-  const {sets, cards} = cockatrice_carddatabase;
-  const jsonSets = {};
-  sets.set.forEach((s) => {
-    jsonSets[s.name] = {
+
+  const {sets, cards} = root;
+  if (!sets || !cards) {
+    throw new Error("node <sets> and <cards> must be present");
+  }
+
+
+  if (!sets.set) {
+    throw new Error("node <sets> must be made of <set>");
+  }
+
+  if (typeof sets.set === "object" && !Array.isArray(sets.set)) {
+    sets.set = [sets.set];
+  }
+
+  if (!cards.card) {
+    throw new Error("node <cards> must have an array of <card>");
+  }
+
+  if (typeof cards.card === "object"  && !Array.isArray(cards.card)) {
+    cards.card = [cards.card];
+  }
+
+  const jsonSets = sets.set.reduce((acc, s) => {
+    acc[s.name] = {
       code: s.name,
       name: s.longname,
       type: s.settype,
       releaseDate: s.releasedate,
-      cards: []
+      cards: [],
+      baseSetSize: 0
     };
-  });
+    return acc;
+  }, {});
 
   cards.card.forEach(c => {
-    if (jsonSets[c.set]) {
-      // const [power, toughness] = c.pt.split("/");
-      // jsonSets[c.set].push({
-      //   name: c.name,
-      //   manaCost: c.manacost,
-      //   cmc: c.cmc,
-      //   loyalty: parseInt(c.loyalty),
-      //   text: c.text,
-      //   type: c.type,
-      //   types: c.types,
-      //   rarity: c.rarity,
-      //   layout: c.layout,
-      //   number: parseInt(c.number),
-      //   supertypes : [],
-      //   scryfallId, side, isAlternative, colors: c.color.split(""),
-      // });
+    const { text: setCode, num, picurl, rarity } = c.set;
+    if ([setCode, num, picurl, rarity].includes(undefined)) {
+      throw new Error("<set> property of <card> must contain text, num, picurl and rarity");
+    }
+    if (jsonSets[setCode]) {
+      const { cmc, colors, layout, loyalty, manacost, pt, side, type } = c.prop;
+      if ([cmc, colors, layout, loyalty, manacost, pt, side, type].includes(undefined)) {
+        throw new Error("<prop> property of <card> must contain cmc, colors, layout, loyalty, manacost, pt, side and type");
+      }
+      const [power, toughness] = pt.split("/");
+      const set = jsonSets[setCode];
+      set.cards.push({
+        name: c.name,
+        manaCost: manacost,
+        cmc: cmc,
+        loyalty: parseInt(loyalty),
+        text: c.text,
+        type: type.toLowerCase(),
+        types: [type.toLowerCase()],
+        rarity,
+        power: parseInt(power) || "",
+        toughness: parseInt(toughness) || "",
+        url: picurl,
+        layout,
+        number: parseInt(num),
+        supertypes : [],
+        side,
+        isAlternative: false,
+        colors: colors.split("")
+      });
+      set.baseSetSize = set.baseSetSize + 1;
     }
   });
-  return;
-}
 
+  return jsonSets;
+}
 
 module.exports = {
   parse
