d3d2867dadae5ab0124ae53dfcec7b5df98c8721
HerveH44 <herve.huneau@gmail.com>
2020-01-01 22:24:37 +0100

    fix the booster value to determine the size of the booster

--- src/make/doSet.js
+++ src/make/doSet.js
@@ -2,10 +2,10 @@ const toBoosterCard = require("./toBoosterCard");
 const groupCardNamesByRarity = require("./groupCardNamesByRarity");
 
 // TODO: this set should return a set or cards?
-function doSet({code, baseSetSize, name, type, releaseDate, booster, cards: mtgJsonCards}) {
+function doSet({code, baseSetSize, name, type, releaseDate, boosterV3, cards: mtgJsonCards}) {
   const cards = mtgJsonCards.reduce(toBoosterCard(code), {});
   const cardNamesByRarity = groupCardNamesByRarity(baseSetSize, Object.values(cards));
-  const size = !booster ? 4 : booster.filter(x => x === "common").length;
+  const size = !boosterV3 ? 4 : boosterV3.filter(x => x === "common").length;
 
   return [{
     name,
