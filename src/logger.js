9c615260769427e3baa7615337ea6a58b4ec6142
Hervé Huneau <herve.huneau@emaze.net>
2020-01-08 12:45:29 +0100

    add config of logger

--- src/logger.js
+++ src/logger.js
@@ -1,31 +1,24 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, printf } = format;
-const { LOGDIR } = require("../config.server");
-const fs = require( "fs" );
-
-if ( !fs.existsSync( LOGDIR ) ) {
-  fs.mkdirSync( LOGDIR );
-}
+const { logger: config } = require("../config");
 
 const logger = createLogger({
-  level: "info",
+  level: config.level,
   format: combine(
     timestamp({ format: "YYYY-MM-DD HH:mm:ss,SSSZZ" }),
     printf(info => `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`),
   ),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new transports.File({ filename: LOGDIR + "error.log", level: "error" }),
-    new transports.File({ filename: LOGDIR + "combined.log" }),
-  ]
 });
 
-// If we're not in production then log to the `console`
-if (process.env.NODE_ENV !== "production") {
+if (config.consoleEnabled) {
   logger.add(new transports.Console());
+}
+
+if (config.file.enabled) {
+  logger.add(new transports.File({
+    tailable: true,
+    ...config.file,
+  }));
 }
 
 module.exports = logger;
