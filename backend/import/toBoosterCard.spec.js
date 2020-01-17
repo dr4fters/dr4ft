440e7cc5e9d46a209b553f86affb44f1d44305a4
HerveH44 <herve.huneau@gmail.com>
2020-01-17 10:08:48 +0100

    add test

--- backend/import/toBoosterCard.spec.js
+++ backend/import/toBoosterCard.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env node, mocha */
-const toBoosterCard = require("./toBoosterCard");
 const fs = require("fs");
+const assert = require("assert");
+const toBoosterCard = require("./toBoosterCard");
 
 describe("Acceptance tests for toBoosterCard function", () => {
   describe("can parse all the sets", () => {
@@ -12,8 +13,11 @@ describe("Acceptance tests for toBoosterCard function", () => {
             return;
           }
           const path = `data/sets/${file}`;
+          const [setCode] = file.split(".");
           const json = JSON.parse(fs.readFileSync(path, "UTF-8"));
-          json.cards.reduce(toBoosterCard, {});
+          json.cards
+            .map(toBoosterCard(setCode))
+            .forEach((card) => assert(card.name != undefined, "card must have a name"));
         });
       }
     }).timeout(10000);
@@ -24,7 +28,67 @@ describe("Acceptance tests for toBoosterCard function", () => {
     it("parse a DFC card as doubleFaced and with a flippedCardURL", () => {});
     it("parse a transform card as doubleFaced and with a flippedCardURL", () => {});
     it("parse a split|aftermath|adventure card with double name", () => {});
-    it("parse a split card with correct mana cost", () => {});
+    it("parse a split card with correct mana cost", () => {
+      const down = {
+        "colorIdentity": ["B", "G"],
+        "colors": ["B"],
+        "convertedManaCost": 7.0,
+        "faceConvertedManaCost": 4.0,
+        "foreignData": [],
+        "frameVersion": "2003",
+        "layout": "split",
+        "manaCost": "{3}{B}",
+        "multiverseId": 369089,
+        "name": "Down",
+        "names": ["Down", "Dirty"],
+        "number": "126",
+        "otherFaceIds": ["f1701b93-065a-54ca-9a96-a924b7312801"],
+        "printings": ["DGM"],
+        "rarity": "uncommon",
+        "scryfallId": "c35c63c1-6344-4d8c-8f7d-cd253d12f9ae",
+        "scryfallIllustrationId": "896e9aff-0430-4912-9a87-2f6377252145",
+        "scryfallOracleId": "eba21e3b-e2b2-4e0b-82e3-f0849943fd89",
+        "side": "a",
+        "subtypes": [],
+        "supertypes": [],
+        "text": "Target player discards two cards.\nFuse (You may cast one or both halves of this card from your hand.)",
+        "type": "Sorcery",
+        "types": ["Sorcery"],
+        "uuid": "c6875787-16eb-5199-bd1e-586dd759171e",
+      };
+      const dirty = {
+        "colorIdentity": ["B", "G"],
+        "colors": ["G"],
+        "convertedManaCost": 7.0,
+        "faceConvertedManaCost": 3.0,
+        "foreignData": [],
+        "layout": "split",
+        "manaCost": "{2}{G}",
+        "mcmId": 261576,
+        "mcmMetaId": 207993,
+        "mtgoId": 48508,
+        "multiverseId": 369089,
+        "name": "Dirty",
+        "names": ["Down", "Dirty"],
+        "number": "126",
+        "otherFaceIds": ["c6875787-16eb-5199-bd1e-586dd759171e"],
+        "printings": ["DGM"],
+        "rarity": "uncommon",
+        "scryfallId": "c35c63c1-6344-4d8c-8f7d-cd253d12f9ae",
+        "side": "b",
+        "subtypes": [],
+        "supertypes": [],
+        "tcgplayerProductId": 67989,
+        "text": "Return target card from your graveyard to your hand.\nFuse (You may cast one or both halves of this card from your hand.)",
+        "type": "Sorcery",
+        "types": ["Sorcery"],
+        "uuid": "f1701b93-065a-54ca-9a96-a924b7312801",
+      };
+
+      const [downParsed, dirtyParsed] = [down, dirty].map(toBoosterCard("setCode"));
+      assert(downParsed.cmc == 7, "split card CMC must equal to both side CMC");
+      assert(dirtyParsed.cmc == 7, "split card CMC must equal to both side CMC");
+    });
     it("parse a multicolor card as multicolor", () => {});
     it("parse a no color card as colorless", () => {});
   });
