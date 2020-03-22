a98b1f81ee17ed2289a6472bb09b5b43c550e8d2
HerveH44 <herve.huneau@gmail.com>
2020-03-22 22:48:18 +0100

    fix: filter alternative cards

--- backend/import/doSet.js
+++ backend/import/doSet.js
@@ -3,7 +3,9 @@ const { keyCardsUuidByNumber, groupCardsUuidByRarity, keyCardsByUuid} = require(
 
 // TODO: this set should return a set or cards?
 function doSet({code, baseSetSize, name, type, releaseDate, boosterV3, cards: mtgJsonCards}) {
-  const cards = mtgJsonCards.map(toBoosterCard(code));
+  const cards = mtgJsonCards
+    .filter((card) => !card.isAlternative)
+    .map(toBoosterCard(code));
   const size = !boosterV3 ? 4 : boosterV3.filter(x => x === "common").length;
 
   return [{
