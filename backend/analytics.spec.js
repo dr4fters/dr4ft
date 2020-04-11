2d3579605040829a081a8d4acffc8b49ce848f79
May <may@Mays-iMac-Pro.local>
2020-04-11 15:36:48 -0700

    Working on specs for anayltics.js

---
+++ backend/analytics.spec.js
@@ -0,0 +1,28 @@
+// List of potentially problematic cards to test over for pack analysis.
+const NyleasIntervention = {
+  uuid: '576e9e04-acd7-5d24-bc19-1dd765e9d1b8',
+  name: "Purphoros's Intervention",
+  names: [],
+  color: 'Red',
+  colors: [ 'R' ],
+  colorIdentity: [ 'R' ],
+  setCode: 'THB',
+  scryfallId: 'ecc911ee-0e12-4b10-add7-9a9d63c29443',
+  cmc: 1,
+  number: '151',
+  type: 'Sorcery',
+  manaCost: '{X}{R}',
+  rarity: 'Rare',
+  url: 'https://api.scryfall.com/cards/ecc911ee-0e12-4b10-add7-9a9d63c29443?format=image',
+  layout: 'normal',
+  isDoubleFaced: false,
+  flippedCardURL: '',
+  supertypes: [],
+  subtypes: [],
+  text: 'Choose one —\n' +
+    '• Create an X/1 red Elemental creature token with trample and haste. Sacrifice it at the beginning of the next end step.\n' +
+    "• Purphoros's Intervention deals twice X damage to target creature or planeswalker.",
+  foil: true
+};
+
+const cardExamples = [];
