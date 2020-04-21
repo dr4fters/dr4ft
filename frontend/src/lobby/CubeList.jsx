b62fee1f8aa80864a07419efa089682d0e6b8a67
HerveH44 <herve.huneau@gmail.com>
2020-04-21 15:20:38 +0200

    add cubecobra integration

--- frontend/src/lobby/CubeList.jsx
+++ frontend/src/lobby/CubeList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 
 import { Textarea } from "../utils";
@@ -9,12 +9,12 @@ const CubeList = () => {
   const [, selectCube] = useState("none");
   let cubeOptions = [];
 
-  const getCubes = async function() {
-    const {data: cubes} = await axios.get("/api/cubes");
+  const getCubes = async () => {
+    const { data: cubes } = await axios.get("/api/cubes");
     setCubes(cubes);
   };
 
-  useEffect(() => {!cubes && getCubes();});
+  useEffect(() => { !cubes && getCubes(); });
 
   Object.keys(cubes).forEach(key => {
     const option = <option value={key} key={key}>{key}</option>;
@@ -24,7 +24,7 @@ const CubeList = () => {
   const handleChange = (e) => {
     e.preventDefault();
     selectCube(e.target.value);
-    App.set( {
+    App.set({
       list: e.target.value === "none" ? "" : cubes[e.target.value]
     });
   };
@@ -37,16 +37,47 @@ const CubeList = () => {
         link='list'
       />
     </div>
-    {cubes && <div id='preset-cubes'>
-      <div>Preset Cubes:</div>
-      <select onChange={handleChange}>
-        <option value='none' key='none'>None</option>
-        <optgroup label="Cube Tutor">
-          {cubeOptions}
-        </optgroup>
-      </select>
-    </div>}
+    <div className='column'>
+      {cubes && <div id='preset-cubes'>
+        <div>Preset Cubes:</div>
+        <select onChange={handleChange}>
+          <option value='none' key='none'>None</option>
+          <optgroup label="Cube Tutor">
+            {cubeOptions}
+          </optgroup>
+        </select>
+      </div>}
+      <CubeCobra />
+    </div>
   </div>;
+};
+
+const CubeCobra = () => {
+  const [name, setName] = useState("");
+
+  const getCubeCobraList = async (cubeId) => {
+    axios.get(`https://cubecobra.com/cube/api/cubelist/${cubeId}`)
+      .then(({ data:list }) => {
+        App.set({ list });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Fragment >
+      <label style={{marginTop: "10px", marginLeft: "4px"}}>
+        <a href="https://cubecobra.com/" >CubeCobra</a> CubeId:{" "}
+        <input
+          style={{ width: "100px" }}
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)} />
+      </label>
+      <input style={{marginTop: "5px", marginLeft: "4px"}} type="button" value="Fetch Cubelist" onClick={() => getCubeCobraList(name)} />
+    </Fragment>
+  );
 };
 
 export default CubeList;
