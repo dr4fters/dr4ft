4c6debc56867d92d63ebe1258e1c70d8e205a3ee
Hervé Huneau <herve.huneau@gmail.com>
2020-04-22 17:32:34 +0200

    add cubecobra integration (#984)

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,12 +2,10 @@
 const express = require("express");
 const games = require("./games");
 const sets = require("./sets");
-const cubes = require("./cubes");
 const apiRouter = express.Router();
 
 apiRouter
   .use("/games", games)
-  .use("/sets", sets)
-  .use("/cubes", cubes);
+  .use("/sets", sets);
 
 module.exports = apiRouter;
