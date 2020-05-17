8953987ebf3c42524ad65d50755e50efdc78d187
tooomm <tooomm@users.noreply.github.com>
2020-05-17 15:15:46 +0200

    wording

--- frontend/src/lobby/Version.jsx
+++ frontend/src/lobby/Version.jsx
@@ -17,7 +17,7 @@ const Version = ({version, MTGJSONVersion, boosterRulesVersion}) => {
       card data {" "}
       <a href={`https://mtgjson.com/changelog/#_${MTGJSONVersion.version.replace(/\./g, "-")}`}>
         v{MTGJSONVersion.version}
-      </a> ({MTGJSONVersion.date}) - Using <a href={"https://github.com/taw/magic-sealed-data"}>Magic Sealed Data</a> {" "}
+      </a> ({MTGJSONVersion.date}) and <a href={"https://github.com/taw/magic-sealed-data"}>Magic Sealed Data</a> {" "}
         booster rules{" "}
       commit <a href={`https://github.com/taw/magic-sealed-data/commit/${boosterRulesVersion}`}>{boosterRulesVersion}</a>
     </p>
