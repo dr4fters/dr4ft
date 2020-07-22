0996dcd9bade5c55b6f5b63ee06aae1697430a97
Blade Barringer <blade.barringer@getbraintree.com>
2020-07-21 19:27:00 -0500

    First draft at modal (#1136)

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+import ReactTooltip from "react-tooltip";
+
+const Modal = ({
+  headerText,
+  footerConfirmButtonText,
+  footerCancelButtonText,
+  show,
+  onClose,
+  onConfirm,
+  children,
+}) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div onScroll={() => {ReactTooltip.rebuild();}} className="modal-outer-container">
+      <div className="modal-backdrop" onClick={onClose}></div>
+      <div className="modal-container">
+        <header className="modal-header">
+          <h2>{headerText}</h2>
+
+          <div className="modal-close-button" onClick={onClose}>
+            <svg width="15px" height="15px" viewBox="0 0 15 15" version="1.1">
+              <title>close modal button</title>
+              <defs>
+                <rect id="path-1" x="240" y="94" width="960" height="848" rx="4"></rect>
+              </defs>
+              <g transform="translate(-1151.000000, -123.000000)">
+                <g transform="translate(240.000000, 118.000000)">
+                  <g transform="translate(909.000000, 3.000000)">
+                    <path d="M3.73748506,2.29810531 L9.5,8.061 L15.2625149,2.29810531 C15.6599887,1.90063156 16.304421,1.90063156 16.7018947,2.29810531 C17.0993684,2.69557905 17.0993684,3.34001131 16.7018947,3.73748506 L10.939,9.5 L16.7018947,15.2625149 C17.0993684,15.6599887 17.0993684,16.304421 16.7018947,16.7018947 C16.304421,17.0993684 15.6599887,17.0993684 15.2625149,16.7018947 L9.5,10.939 L3.73748506,16.7018947 C3.34001131,17.0993684 2.69557905,17.0993684 2.29810531,16.7018947 C1.90063156,16.304421 1.90063156,15.6599887 2.29810531,15.2625149 L8.061,9.5 L2.29810531,3.73748506 C1.90063156,3.34001131 1.90063156,2.69557905 2.29810531,2.29810531 C2.69557905,1.90063156 3.34001131,1.90063156 3.73748506,2.29810531 Z" fill="#079DFF"></path>
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </div>
+        </header>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <button onClick={onClose}>{footerCancelButtonText || "Cancel"}</button>
+          <button onClick={onConfirm} className="primary">{footerConfirmButtonText || "Confirm"}</button>
+        </div>
+      </div>
+
+      <ReactTooltip className="modal-tooltip" effect="solid" type="info" place="bottom" backgroundColor='#079DFF' textColor='#202124' />
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  headerText: PropTypes.string.isRequired,
+  footerConfirmButtonText: PropTypes.string,
+  footerCancelButtonText: PropTypes.string,
+  show: PropTypes.bool,
+
+  onClose: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+
+  children: PropTypes.node.isRequired
+};
+
+export default Modal;
--- frontend/src/lobby/CubeList.jsx
+++ frontend/src/lobby/CubeList.jsx
@@ -5,28 +5,39 @@ import TextArea from "../components/TextArea";
 import App from "../app";
 
 const CubeList = () => {
-  const cubeListLength =
-    App.state.list.length === 0
-      ? 0
-      : App.state.list.split("\n").length;
-
   return (<div id='cube-list'>
-    <div className='column'>
-      <div>{`One card per line! (${cubeListLength} cards)`}</div>
-      <TextArea className="cube-list"
-        placeholder='cube list'
-        link='list'
-      />
-    </div>
-    <div className='column'>
+    <div>
       <CubeCobra />
+    </div>
+    <br />
+    <div>
+      <ManualCubeInput />
     </div>
   </div>
   );
 };
 
+const ManualCubeInput = () => {
+  const cubeListLength =
+    App.state.list.length === 0
+      ? 0
+      : App.state.list.split("\n").length;
+
+  return (
+    <Fragment>
+      <div>{`Or copy and paste your cube. One card per line! (${cubeListLength} cards)`}</div>
+      <TextArea className="cube-list"
+        placeholder='cube list'
+        link='list'
+      />
+    </Fragment>
+  );
+};
+
 const CubeCobra = () => {
   const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [cubeImportMessage, setCubeImportMessage] = useState("");
 
   const getCubeCobraList = async (cubeId) => {
     if (!cubeId) {
@@ -34,25 +45,43 @@ const CubeCobra = () => {
     }
     axios.get(`https://cubecobra.com/cube/api/cubelist/${cubeId}`)
       .then(({ data: list }) => {
-        App.err = "";
+        setError("");
+        setCubeImportMessage(`The cube with ID "${cubeId}" was imported.`);
         App.save("list", list);
       })
       .catch(() => {
-        App.error(`Could not retrieve CubeCobra list with ID ${cubeId}`);
+        setError(`Could not retrieve CubeCobra list with ID ${cubeId}`);
+        setCubeImportMessage("");
       });
+  };
+
+  const onKeyPress = (e) => {
+    if (event.key !== "Enter"){
+      return;
+    }
+
+    return getCubeCobraList(e.target.value);
   };
 
   return (
     <Fragment >
-      <label style={{ marginTop: "10px", marginLeft: "4px" }}>
-        <a href="https://cubecobra.com/" target="_blank" rel="noopener noreferrer">CubeCobra</a> cube ID:{" "}
+      <div>
+        Import from <a href="https://cubecobra.com/" target="_blank" rel="noopener noreferrer">CubeCobra</a>. Paste the ID for your cube and then press enter.
+      </div>
+      <label>
         <input
-          style={{ width: "100px" }}
           type='text'
           value={name}
+          className={error ? "error": ""}
+          placeholder="CubeCobra Cube ID"
+          onKeyPress={onKeyPress}
           onChange={(e) => setName(e.target.value)} />
       </label>
-      <input style={{ marginTop: "5px", marginLeft: "4px" }} type="button" value="Fetch Cube List" onClick={() => getCubeCobraList(name)} />
+
+      <div>
+        <small>{cubeImportMessage}</small>
+        <small className="error">{error}</small>
+      </div>
     </Fragment>
   );
 };
