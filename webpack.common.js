312ec6c25712359ec3f269aed71680b2be3fb105
mixmix <whimful@gmail.com>
2020-10-06 12:20:11 +1300

    fix settings radio buttons, add scss support

--- webpack.common.js
+++ webpack.common.js
@@ -67,6 +67,17 @@ module.exports = {
         ]
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/,
         loader: "url-loader"
       }
