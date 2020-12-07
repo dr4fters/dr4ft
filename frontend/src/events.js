50231bab6e758c55a64531ffc495ea50a936a1b2
HerveH44 <herve.huneau@gmail.com>
2020-12-07 14:49:17 +0100

    fix picking

--- frontend/src/events.js
+++ frontend/src/events.js
@@ -383,7 +383,8 @@ const clickPack = (card) => {
   if (!App.state.gameState.isAutopick(card.cardId)) {
     App.state.gameState.updateAutopick(card.cardId, App.state.picksPerPack);
     App.send("autopick", index);
-  } else if (App.state.gameState.isPickReady(App.state.picksPerPack, App.state.game.burnsPerPack)){
+  }
+  if (App.state.gameState.isPickReady(App.state.picksPerPack, App.state.game.burnsPerPack)){
     App.state.gameState.resetPack();
     App.update();
     App.send("pick");
