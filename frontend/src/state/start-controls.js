083d8baa8bc2f290a8d2f7c4aafd3b2d04d744a0
HerveH44 <herve.huneau@gmail.com>
2020-12-10 16:46:05 +0100

    solve spaces pb

--- frontend/src/state/start-controls.js
+++ frontend/src/state/start-controls.js
@@ -3,32 +3,32 @@ import { createSlice } from '@reduxjs/toolkit';
 export const timers = ["Fast", "Moderate", "Slow", "Leisurely"];
 
 export const initialState = {
-    addBots: true,
-    shufflePlayers: true,
-    useTimer: true,
-    timerLength: "Moderate", // Fast Moderate or Slow
+  addBots: true,
+  shufflePlayers: true,
+  useTimer: true,
+  timerLength: "Moderate", // Fast Moderate or Slow
 };
 
 const startControls = createSlice({
-        name: "startControls",
-        initialState,
-        reducers: {
-            toggleBots: state => {
-                state.addBots = !state.addBots;
-            },
-            toggleShufflePlayers: state => {
-                state.shufflePlayers = !state.shufflePlayers;
-            },
-            toggleUseTimer: state => {
-                state.useTimer = !state.useTimer;
-            },
-            setTimerLength: (state, action) => {
-                if (timers.includes(action.payload)) {
-                    state.timerLength = action.payload;
-                }
-            }
-        }
+  name: "startControls",
+  initialState,
+  reducers: {
+    toggleBots: state => {
+      state.addBots = !state.addBots;
+    },
+    toggleShufflePlayers: state => {
+      state.shufflePlayers = !state.shufflePlayers;
+    },
+    toggleUseTimer: state => {
+      state.useTimer = !state.useTimer;
+    },
+    setTimerLength: (state, action) => {
+      if (timers.includes(action.payload)) {
+        state.timerLength = action.payload;
+      }
     }
+  }
+}
 );
 
 export const selectAddBots = state => state.startControls.addBots;
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,42 +1,44 @@
-import {combineReducers, configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage'
-import { persistReducer, FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER } from 'redux-persist';
-import {initialState as startControlInitState} from "./start-controls";
+import {
+  persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import { initialState as startControlInitState } from "./start-controls";
 import startControls from "./start-controls";
 import migrateState from './migration';
 
 const reducers = combineReducers({
-    startControls: startControls.reducer
+  startControls: startControls.reducer
 });
 
 const persistConfig = {
-    key: "root",
-    storage,
-    migrate: (state) => {
-        if (!state) { 
-            state = {};
-        }
-        const newState = {
-            ...state,
-            startControls: migrateState(startControlInitState, state.startControls)
-        };
-        return Promise.resolve(newState);
+  key: "root",
+  storage,
+  migrate: (state) => {
+    if (!state) {
+      state = {};
     }
+    const newState = {
+      ...state,
+      startControls: migrateState(startControlInitState, state.startControls)
+    };
+    return Promise.resolve(newState);
+  }
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-    reducer: persistedReducer,
-    devTools: true,
-    middleware: getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-          }
-      }),
+  reducer: persistedReducer,
+  devTools: true,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  }),
 })
