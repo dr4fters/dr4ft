341fce3bdd4886b4c1ec782a1afc053031fa7a84
tooomm <tooomm@users.noreply.github.com>
2021-02-07 22:19:58 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -20,7 +20,7 @@ export const STRINGS = {
         </div>
         <div>
           Contributions welcome! &nbsp;
-          <a href='https://github.com/dr4fters/dr4ft' target='_blank'>
+          <a href='https://github.com/dr4fters/dr4ft' target='_blank' rel='noreferrer'>
             <img
               src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
               alt='GitHub' title='GitHub Repository' align='top' height='18'
