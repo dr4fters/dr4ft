8af9c979e2937857d0c37f821ab26da6cd12bddc
tooomm <tooomm@users.noreply.github.com>
2021-02-07 21:06:21 +0100

    Update Version.jsx

--- frontend/src/lobby/Version.jsx
+++ frontend/src/lobby/Version.jsx
@@ -14,21 +14,21 @@ const Version = ({version, MTGJSONVersion, boosterRulesVersion}) => {
     <div className="Version">
       <div>
         dr4ft version: {" "}
-        <a href={`https://github.com/dr4fters/dr4ft/${getLink(version)}`} className='code'>
+        <a href={`https://github.com/dr4fters/dr4ft/${getLink(version)} target="_blank`} className='code'>
           <code>{version}</code>
         </a> <span className='date'>({BUILD_DATE})</span>
       </div>
 
       <div>
-        Card data: <a href="https://www.mtgjson.com">MTGJSON</a> {" "}
-        <a href={`https://mtgjson.com/changelog/version-5/#_${MTGJSONVersion.version.replace(/\./g, "-")}`} className='code'>
+        Card data: <a href="https://www.mtgjson.com" target="_blank">MTGJSON</a> {" "}
+        <a href={`https://mtgjson.com/changelog/mtgjson-v5/#_${MTGJSONVersion.version.replace(/\./g, "-")}`} className='code'>
           <code>v{MTGJSONVersion.version}</code>
         </a> <span className='date'>({MTGJSONVersion.date})</span>
       </div>
 
       <div>
         Booster rules: {" "}
-        <a href={"https://github.com/taw/magic-sealed-data"}>Magic Sealed Data</a> {" "}
+        <a href={"https://github.com/taw/magic-sealed-data" target="_blank"}>Magic Sealed Data</a> {" "}
         <a href={`https://github.com/taw/magic-sealed-data/commit/${boosterRulesVersion}`} className='code'>
           <code>{boosterRulesVersion.substring(0, 7)}</code>
         </a>
