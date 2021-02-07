e170f863db9181a2857af3ccb6c1e32bcf72e918
tooomm <tooomm@users.noreply.github.com>
2021-02-07 23:40:37 +0100

    -div

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -13,17 +13,15 @@ export const STRINGS = {
 
     FOOTER:
       <div style={{ padding: 10 }}>
-        <div>
-          Contributions welcome! &nbsp;
-          <a href='https://github.com/dr4fters/dr4ft'>
-            <img
-              src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
-              alt='GitHub' title='GitHub Repository' align='top' height='18'
-              style={{ marginRight: 5}}
-            />
-            dr4fters/dr4ft
-          </a>
-        </div>
+        Contributions welcome! &nbsp;
+        <a href='https://github.com/dr4fters/dr4ft'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
+            alt='GitHub' title='GitHub Repository' align='top' height='18'
+            style={{ marginRight: 5}}
+          />
+          dr4fters/dr4ft
+        </a>
       </div>
   }
 };
