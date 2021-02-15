a89a75b69fd2f6574353afef9928a16be1657541
mixmix <whimful@gmail.com>
2021-02-15 14:16:23 +1300

    MTGO export: drop .dek in favour of .txt export

--- frontend/src/export/mtgo.js
+++ frontend/src/export/mtgo.js
@@ -3,50 +3,23 @@ import { ZONE_MAIN, ZONE_SIDEBOARD } from "../zones";
 export default {
   name: "MTGO",
   download,
-  downloadExtension: ".dek"
+  downloadExtension: ".mtgo.txt"
   /* If mtgo later supports clipboard import, enable this: */
   // copy
 };
 
 function download (name, deck) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<Deck xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <NetDeckID>0</NetDeckID>
-  <PreconstructedDeckID>0</PreconstructedDeckID>
+  return [
+    ...deck[ZONE_MAIN].map(renderDownloadCard),
 
-${
-  deck[ZONE_MAIN]
-    .map(renderDownloadCard)
-    .filter(Boolean)
-    .join("\n")
+    ...(deck[ZONE_SIDEBOARD].length ? ["", "Sideboard"] : []),
+
+    ...deck[ZONE_SIDEBOARD].map(renderDownloadCard)
+  ].join("\n");
 }
 
-${
-  deck[ZONE_SIDEBOARD]
-    .map(renderDownloadCard)
-    .filter(Boolean)
-    .join("\n")
-}
-</Deck>
-`;
-}
-
-function renderDownloadCard ({ card, count, sideboard = false }) {
-  if (!card.identifiers.mtgoId) {
-    console.warn(`Exporting ${card.name} to .dek, it lacks an mtgoId, which may cause error on importing`);
-    return `  <Cards Quantity="${count}" Sideboard="${sideboard}" Name="${correctName(card)}" Annotation="0" />`;
-
-    // NOTE failing to have an mtgoId seems to mainly be because a set isn't present in MTGO.
-    // There are two cases so far known
-    // 1. it's a card that was never in mtgo - e.g. any unset card
-    //   - them import of any .dek containing this card will fail
-    //
-    // 2. it's a card that exists in mtgo, just not this printing - e.g. some promo Ancestral Recall
-    //   - exporting this card without a CatId leads MTGO to select another printing of the card
-    //   - the import of the .dek file works fine
-  }
-
-  return `  <Cards CatID="${card.identifiers.mtgoId}" Quantity="${count}" Sideboard="${sideboard}" Name="${correctName(card)}" Annotation="0" />`;
+function renderDownloadCard ({ card, count }) {
+  return `${count} ${correctName(card)}`;
 }
 
 function correctName (card) {
@@ -65,3 +38,48 @@ function correctName (card) {
     return card.name;
   }
 }
+
+// NOTE: the following was abandoned because supporting .dek was hard
+// and MTGO seems to handle a lot of tricky edge cases with .txt import
+
+// function download (name, deck) {
+//   return `<?xml version="1.0" encoding="UTF-8"?>
+// <Deck xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+//   <NetDeckID>0</NetDeckID>
+//   <PreconstructedDeckID>0</PreconstructedDeckID>
+
+// ${
+//   deck[ZONE_MAIN]
+//     .map(renderDownloadCard)
+//     .filter(Boolean)
+//     .join("\n")
+// }
+
+// ${
+//   deck[ZONE_SIDEBOARD]
+//     .map(renderDownloadCard)
+//     .filter(Boolean)
+//     .join("\n")
+// }
+// </Deck>
+// `;
+// }
+
+// function renderDownloadCard ({ card, count, sideboard = false }) {
+//   if (!card.identifiers.mtgoId) {
+//     console.warn(`Exporting ${card.name} to .dek, it lacks an mtgoId, which may cause error on importing`);
+//     return `  <Cards Quantity="${count}" Sideboard="${sideboard}" Name="${correctName(card)}" Annotation="0" />`;
+
+//     // NOTE failing to have an mtgoId seems to mainly be because a set isn't present in MTGO.
+//     // There are two cases so far known
+//     // 1. it's a card that was never in mtgo - e.g. any unset card
+//     //   - them import of any .dek containing this card will fail
+//     //
+//     // 2. it's a card that exists in mtgo, just not this printing - e.g. some promo Ancestral Recall
+//     //   - exporting this card without a CatId leads MTGO to select another printing of the card
+//     //   - the import of the .dek file works fine
+//   }
+
+//   return `  <Cards CatID="${card.identifiers.mtgoId}" Quantity="${count}" Sideboard="${sideboard}" Name="${correctName(card)}" Annotation="0" />`;
+// }
+
--- frontend/src/export/mtgo.spec.js
+++ frontend/src/export/mtgo.spec.js
@@ -18,23 +18,15 @@ describe("export/mtgo", () => {
   });
 });
 
-const downloadOutput = () => (
-  `<?xml version="1.0" encoding="UTF-8"?>
-<Deck xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <NetDeckID>0</NetDeckID>
-  <PreconstructedDeckID>0</PreconstructedDeckID>
+const downloadOutput = () => {
+  return `2 Nezumi Shortfang
+1 Kessig Prowler
+1 Murderous Rider
+1 Refuse/Cooperate
+1 Riverglide Pathway
+1 Dead/Gone
 
-  <Cards CatID="21213" Quantity="2" Sideboard="false" Name="Nezumi Shortfang" Annotation="0" />
-  <Cards CatID="61446" Quantity="1" Sideboard="false" Name="Kessig Prowler" Annotation="0" />
-  <Cards CatID="78308" Quantity="1" Sideboard="false" Name="Murderous Rider" Annotation="0" />
-  <Cards CatID="64832" Quantity="1" Sideboard="false" Name="Refuse/Cooperate" Annotation="0" />
-  <Cards CatID="83533" Quantity="1" Sideboard="false" Name="Riverglide Pathway" Annotation="0" />
-  <Cards CatID="26517" Quantity="1" Sideboard="false" Name="Dead/Gone" Annotation="0" />
-
-  <Cards CatID="48512" Quantity="1" Sideboard="true" Name="Catch/Release" Annotation="0" />
-  <Cards Quantity="1" Sideboard="true" Name="Who/What/When/Where/Why" Annotation="0" />
-</Deck>
-`);
-
-// NOTE - that we've dropped cards which don't have mtgoId
-// They probably don't have this identifier because not supported by MTGO (e.g. unset cards)
+Sideboard
+1 Catch/Release
+1 Who/What/When/Where/Why`;
+};
