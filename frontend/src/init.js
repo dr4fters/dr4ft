166d788e3fac06083f946bb32b8ac82ac3ec4cee
mixmix <whimful@gmail.com>
2021-03-01 00:21:00 +1300

    change set-picker for a fuzzy-search pikcer with icons!

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,6 +2,8 @@ import App from "./app";
 import "./events";
 import { default as router } from "./router";
 
+import "./react-select-search.scss";
+
 App.init(router);
 
 if (module.hot) {
--- frontend/src/lobby/Set.jsx
+++ frontend/src/lobby/Set.jsx
@@ -1,23 +1,85 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SelectSearch from 'react-select-search';
+import Fuse from 'fuse.js';
+
+function fuzzySearch (options) {
+  return value => {
+    if (!value.length) {
+      return options;
+    }
+
+    return options.reduce((acc, group) => {
+      const fuse = new Fuse(group.items, {
+        keys: ['name', 'value'],
+        minMatchCharLength: 2,
+        threshold: 0.3
+      })
+      const matches = fuse.search(value)
+
+      if (matches.length) {
+        acc.push({
+          ...group,
+          items: matches
+        })
+      }
+      return acc
+    }, [])
+  };
+}
 
 import App from "../app";
-import SetChoices from "./SetChoices";
 
 const Set = ({ index, selectedSet, type }) => {
-  const onSetChange = (e) => {
-    const chosenSet = e.currentTarget.value;
-    let sets = App.state[type];
-    sets[index] = chosenSet;
+  const options = Object.entries(App.state.availableSets).reduce((acc, [setType, sets]) => {
+    acc.push({
+      type: 'group',
+      name: setType, // TODO titlecase
+      items: sets.map(set => ({
+        name: set.name,
+        value: set.code
+      }))
+    })
+
+    return acc
+  }, [])
+
+  const handleSetChange = (value) => {
+    const sets = App.state[type];
+    sets[index] = value;
+
     App.save(type, App.state[type]);
   };
+
   return (
-    <div>
-      {/* TODO pull down set icons */}
-      <select value={selectedSet} onChange={onSetChange} key={index}>
-        <SetChoices/>
-      </select>
-    </div>
+    <SelectSearch
+      options={options}
+      name="card-set"
+      placeholder="Choose a set"
+      value={selectedSet}
+      onChange={handleSetChange}
+      search
+      filterOptions={fuzzySearch}
+      // renderValue={(valueProps, ref, selectedValue) => {
+      //   return (
+      //     <div>
+      //       <i className={`ss ss-${App.state[type][index].toLowerCase()}`} />
+      //       <input className='select-search__input' {...valueProps} />
+      //     </div>
+      //   )
+      // }}
+      renderOption={(optionProps, optionData) => {
+        return (
+          <button className='select-search__option' {...optionProps}>
+            <i className={`ss ss-${optionData.value.toLowerCase()}`} />
+            {optionData.name}
+            <span className='set-code'>
+              {optionData.value}
+            </span>
+          </button>
+        )
+      }}
+    />
   );
 };
 
