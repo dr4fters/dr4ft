4e77c0d55ea01319a254ba33908bf5d7c4310efe
ZeldaZach <zahalpern+github@gmail.com>
2022-02-26 01:30:56 -0500

    upgrade to webpack 5

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map"
+  mode: "development"
 });
