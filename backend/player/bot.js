6f836f089455fc254d2b9b9854881023b55483b4
mix irving <mix@protozoa.nz>
2023-04-26 09:24:45 +1200

    linting

--- backend/player/bot.js
+++ backend/player/bot.js
@@ -1,20 +1,20 @@
 const { sample, pull, times, shuffle } = require("lodash");
 const pileSort = require("pile-sort");
 
-const RARITY_ORDER = ['Mythic', 'Rare', 'Uncommon', 'Common', 'Basic'];
+const RARITY_ORDER = ["Mythic", "Rare", "Uncommon", "Common", "Basic"];
 function sortByRarity (cards) {
   return shuffle(cards).sort((a, b) => {
     return RARITY_ORDER.indexOf(a.rarity) - RARITY_ORDER.indexOf(b.rarity);
-  })
+  });
 }
 
-const decidingPicksNumber = 6
+const decidingPicksNumber = 6;
 function samplePick (pack, pool, myColors) {
   if (pool.length === decidingPicksNumber) {
     const colorCount = pool.reduce(
       (acc, pack) => {
-        pack.colorIdentity.forEach(color => { acc[color] += 1 })
-        return acc
+        pack.colorIdentity.forEach(color => { acc[color] += 1; });
+        return acc;
       },
       {
         W: 0,
@@ -24,29 +24,29 @@ function samplePick (pack, pool, myColors) {
         G: 0
         // TODO count colourless?
       }
-    )
+    );
 
     const countColors = Object.keys(colorCount).reduce(
       (acc, color) => {
-        const count = colorCount[color]
+        const count = colorCount[color];
         if (!acc[count]) {
-          acc[count] = []
+          acc[count] = [];
         }
-        acc[count].push(color)
-        return acc
+        acc[count].push(color);
+        return acc;
       },
       {}
-    )
+    );
     const orderedColors = Object.values(countColors)
       .reduce(
         (acc, next) => {
-          return [...acc, ...shuffle(next)]
+          return [...acc, ...shuffle(next)];
         },
         []
-      )
+      );
 
-    myColors.clear()
-    orderedColors.slice(0, 2).forEach(color => myColors.add(color))
+    myColors.clear();
+    orderedColors.slice(0, 2).forEach(color => myColors.add(color));
   }
 
   const [cardsInMyColors, cardsNotInMyColors] = pileSort(pack, [
@@ -56,14 +56,14 @@ function samplePick (pack, pool, myColors) {
         card.colorIdentity.some(color => myColors.has(color))
         // NOTE: this means if you if your colors are WU, you can find your bot
         // picks a WG card (they match on W and are splashing?)
-      )
+      );
     }
-  ])
+  ]);
 
-  const orderedPicks = sortByRarity(cardsInMyColors)
-  if (orderedPicks.length) return orderedPicks[0]
+  const orderedPicks = sortByRarity(cardsInMyColors);
+  if (orderedPicks.length) return orderedPicks[0];
 
-  return sortByRarity(cardsNotInMyColors)[0]
+  return sortByRarity(cardsNotInMyColors)[0];
 }
 
 const Player = require("./index");
@@ -81,7 +81,7 @@ module.exports = class Bot extends Player {
     this.picksPerPack = picksPerPack;
     this.burnsPerPack = burnsPerPack;
 
-    this.myColors = new Set(['W', 'U', 'B', 'R', 'G']) // bot chooses 2 colors at some point
+    this.myColors = new Set(["W", "U", "B", "R", "G"]); // bot chooses 2 colors at some point
   }
 
   getPack(pack) {
